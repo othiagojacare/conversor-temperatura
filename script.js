47906f7a2b52dd667e0441bcee8913a5c56aaff8
Thiago <122575310+othiagojacare@users.noreply.github.com>
2024-02-13 19:36:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function converte(){
     var temp = document.getElementById('temp').value  
+    var escala = document.getElementById('temp_select').value
 
     if(temp == 0){
         alert('Digite um valor')
+    }if(escala == 'Fahrenheit'){
+        var res = (temp - 32) * 5/9;
+        alert(`A temperatura de Fahrenheit em Celsius é de ${res.toFixed(2)} graus `)
     }else{
-    var res = (temp - 32) * 5/9;
-    alert(`A temperatura em Celsius é de ${res.toFixed(2)} graus `)
+        res = (temp - 273,15);
+        alert(`A temperatura de Kelvin em Celsius é de ${res.toFixed(2)} graus `)
     }    
 }
+
 
